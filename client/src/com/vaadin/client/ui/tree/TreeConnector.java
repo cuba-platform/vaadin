072bd8c9837467e9451dae59a48a28db5e0c07dd
Yuriy Artamonov <artamonov@haulmont.com>
2013-06-18 16:01:00 +0400

    Tree - Haulmont protected API

--- client/src/com/vaadin/client/ui/tree/TreeConnector.java
+++ client/src/com/vaadin/client/ui/tree/TreeConnector.java
@@ -103,7 +103,8 @@ public class TreeConnector extends AbstractComponentConnector implements
                 getWidget().updateDropHandler(childUidl);
                 continue;
             }
-            childTree = getWidget().new TreeNode();
+            // Haulmont API
+            childTree = createNode(childUidl);
             getConnection().getVTooltip().connectHandlersToWidget(childTree);
             updateNodeFromUIDL(childTree, childUidl, 1);
             getWidget().body.add(childTree);
@@ -180,6 +181,11 @@ public class TreeConnector extends AbstractComponentConnector implements
 
         getWidget().rendering = false;
 
+    }
+
+    // Haulmont API
+    protected TreeNode createNode(UIDL childUidl) {
+        return getWidget().new TreeNode();
     }
 
     @Override
@@ -306,7 +312,8 @@ public class TreeConnector extends AbstractComponentConnector implements
                 updateActionMap(childUidl);
                 continue;
             }
-            final TreeNode childTree = getWidget().new TreeNode();
+            // Haulmont API
+            final TreeNode childTree = createNode(childUidl);
             getConnection().getVTooltip().connectHandlersToWidget(childTree);
             updateNodeFromUIDL(childTree, childUidl, level);
             containerNode.childNodeContainer.add(childTree);
