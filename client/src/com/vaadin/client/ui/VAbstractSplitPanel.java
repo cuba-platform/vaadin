a72c505964888c9d80283ad29811a3038e4eb1aa
Yuriy Artamonov <artamonov@haulmont.com>
2013-08-06 12:40:35 +0400

    Haulmont protected API for SplitPanel extension

--- client/src/com/vaadin/client/ui/VAbstractSplitPanel.java
+++ client/src/com/vaadin/client/ui/VAbstractSplitPanel.java
@@ -756,7 +756,8 @@ public class VAbstractSplitPanel extends ComplexPanel {
 
     private int splitterSize = -1;
 
-    private int getSplitterSize() {
+    // Haulmont API dependency
+    protected int getSplitterSize() {
         if (splitterSize < 0) {
             if (isAttached()) {
                 switch (orientation) {
