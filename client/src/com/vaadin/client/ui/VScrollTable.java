df659c138f1871691e4e55ae77ced00492670673
Yuriy Artamonov <artamonov@haulmont.com>
2013-10-10 18:47:48 +0400

    VScrollTable - protected API to adjust table height by content

--- client/src/com/vaadin/client/ui/VScrollTable.java
+++ client/src/com/vaadin/client/ui/VScrollTable.java
@@ -2500,8 +2500,8 @@ public class VScrollTable extends FlowPanel implements HasWidgets,
                 // int bodyHeight = scrollBody.getOffsetHeight();
                 bodyHeight = scrollBody.getRequiredHeight();
             } else {
-                bodyHeight = (int) Math.round(scrollBody.getRowHeight(true)
-                        * pageLength);
+                // Haulmont API extracted method
+                bodyHeight = getDynamicBodyHeight();
             }
             boolean needsSpaceForHorizontalSrollbar = (total > availW);
             if (needsSpaceForHorizontalSrollbar) {
@@ -2543,6 +2543,12 @@ public class VScrollTable extends FlowPanel implements HasWidgets,
                 .getElement());
 
         hadScrollBars = willHaveScrollbarz;
+    }
+
+    // Haulmont API
+    protected int getDynamicBodyHeight() {
+        return (int) Math.round(scrollBody.getRowHeight(true)
+                * pageLength);
     }
 
     /**
