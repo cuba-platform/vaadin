3eb903ae5bc91dc693e3a68f4f4a2ee98f3f2107
Yuriy Artamonov <artamonov@haulmont.com>
2015-03-18 13:46:17 +0400

    Fix for #VAADIN-12970, fix moved to platform CubaScrollTableWidget/CubaTreeTableWidget

--- client/src/com/vaadin/client/ui/VScrollTable.java
+++ client/src/com/vaadin/client/ui/VScrollTable.java
@@ -92,6 +92,7 @@ import com.vaadin.client.Util;
 import com.vaadin.client.VConsole;
 import com.vaadin.client.VTooltip;
 import com.vaadin.client.WidgetUtil;
+import com.vaadin.client.*;
 import com.vaadin.client.ui.VScrollTable.VScrollTableBody.VScrollTableRow;
 import com.vaadin.client.ui.dd.DDUtil;
 import com.vaadin.client.ui.dd.VAbstractDropHandler;
@@ -7230,6 +7231,8 @@ public class VScrollTable extends FlowPanel implements HasWidgets,
                     extraSpace = Math.max(availW - usedMinimumWidth, 0);
                 }
             }
+            // Haulmont API
+            boolean colWidthChanged = false;
 
             // we have some space that can be divided optimally
             HeaderCell hCell;
@@ -7266,11 +7269,15 @@ public class VScrollTable extends FlowPanel implements HasWidgets,
                     checksum += newSpace;
                     setColWidth(colIndex, newSpace, false);
 
+                    colWidthChanged = true;
+
                 } else {
                     if (hierarchyHeaderInNeedOfFurtherHandling == hCell) {
                         // defined with enforced into indent width
                         checksum += hierarchyColumnIndent;
                         setColWidth(colIndex, hierarchyColumnIndent, false);
+                        // Haulmont API
+                        colWidthChanged = true;
                     } else {
                         int cellWidth = hCell.getWidthWithIndent();
                         checksum += cellWidth;
@@ -7278,6 +7285,8 @@ public class VScrollTable extends FlowPanel implements HasWidgets,
                             // update in case the indent has changed
                             // (not detectable earlier)
                             setColWidth(colIndex, cellWidth, true);
+                            // Haulmont API
+                            colWidthChanged = true;
                         }
                     }
                 }
@@ -7297,6 +7306,9 @@ public class VScrollTable extends FlowPanel implements HasWidgets,
                     if (!hc.isResizing && !hc.isDefinedWidth()) {
                         setColWidth(colIndex, hc.getWidthWithIndent() + availW
                                 - checksum, false);
+
+                        // Haulmont API
+                        colWidthChanged = true;
                         break;
                     }
                     colIndex++;
@@ -7320,9 +7332,18 @@ public class VScrollTable extends FlowPanel implements HasWidgets,
             }
 
             forceRealignColumnHeaders();
-        }
 
+            if (colWidthChanged) {
+                // Haulmont API
+                scheduleLayoutForChildWidgets();
+            }
+        }
     };
+
+    // Haulmont API
+    public void scheduleLayoutForChildWidgets() {
+        // implement in descendants
+    }
 
     private void forceRealignColumnHeaders() {
         if (BrowserInfo.get().isIE()) {
--- client/src/com/vaadin/client/ui/table/TableConnector.java
+++ client/src/com/vaadin/client/ui/table/TableConnector.java
@@ -21,7 +21,6 @@ import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style.Position;
-import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.Widget;
 import com.vaadin.client.ApplicationConnection;
 import com.vaadin.client.BrowserInfo;
@@ -34,10 +33,7 @@ import com.vaadin.client.TooltipInfo;
 import com.vaadin.client.UIDL;
 import com.vaadin.client.WidgetUtil;
 import com.vaadin.client.*;
-import com.vaadin.client.ui.AbstractHasComponentsConnector;
-import com.vaadin.client.ui.ManagedLayout;
-import com.vaadin.client.ui.PostLayoutListener;
-import com.vaadin.client.ui.VScrollTable;
+import com.vaadin.client.ui.*;
 import com.vaadin.client.ui.VScrollTable.ContextMenuDetails;
 import com.vaadin.client.ui.VScrollTable.VScrollTableBody.VScrollTableRow;
 import com.vaadin.shared.ui.Connect;
@@ -374,20 +370,8 @@ public class TableConnector extends AbstractHasComponentsConnector implements
                     getLayoutManager().setNeedsVerticalLayout(
                             TableConnector.this);
 
-                    // Fix for #VAADIN-12970, initial layout for cell widgets
                     // Haulmont API
-                    for (Widget w : getWidget().scrollBody) {
-                        HasWidgets row = (HasWidgets) w;
-                        for (Widget child : row) {
-                            ComponentConnector childConnector =
-                                    Util.findConnectorFor(child);
-                            if (childConnector instanceof ManagedLayout) {
-                                LayoutManager lm = getLayoutManager();
-                                lm.setNeedsMeasure(childConnector);
-                                lm.setNeedsLayout((ManagedLayout) childConnector);
-                            }
-                        }
-                    }
+                    getWidget().scheduleLayoutForChildWidgets();
 
                     getLayoutManager().layoutNow();
                 }
