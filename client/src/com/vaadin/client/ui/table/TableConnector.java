c13bc2d272411c3901b8f3b26d063108eacc1455
Yuriy Artamonov <artamonov@haulmont.com>
2015-03-18 13:46:17 +0400

    Fix for #VAADIN-12970, fix moved to platform CubaScrollTableWidget/CubaTreeTableWidget

--- client/src/com/vaadin/client/ui/table/TableConnector.java
+++ client/src/com/vaadin/client/ui/table/TableConnector.java
@@ -24,7 +24,6 @@ import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style.Position;
 import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.Widget;
 import com.vaadin.client.ApplicationConnection;
 import com.vaadin.client.BrowserInfo;
@@ -39,8 +38,6 @@ import com.vaadin.client.TooltipInfo;
 import com.vaadin.client.UIDL;
 import com.vaadin.client.WidgetUtil;
 import com.vaadin.client.ui.AbstractFieldConnector;
-import com.vaadin.client.*;
-import com.vaadin.client.ui.ManagedLayout;
 import com.vaadin.client.ui.PostLayoutListener;
 import com.vaadin.client.ui.VScrollTable;
 import com.vaadin.client.ui.VScrollTable.ContextMenuDetails;
@@ -386,20 +383,8 @@ public class TableConnector extends AbstractFieldConnector implements
                     getLayoutManager().setNeedsVerticalLayout(
                             TableConnector.this);
 
-                    // Fix for #VAADIN-12970, initial layout for cell widgets
                     // Haulmont API
-                    for (Widget w : getWidget().scrollBody) {
-                        HasWidgets row = (HasWidgets) w;
-                        for (Widget child : row) {
-                            ComponentConnector childConnector =
-                                    Util.findConnectorFor(child);
-                            if (childConnector instanceof ManagedLayout) {
-                                LayoutManager lm = getLayoutManager();
-                                lm.setNeedsMeasure(childConnector);
-                                lm.setNeedsLayout((ManagedLayout) childConnector);
-                            }
-                        }
-                    }
+                    getWidget().scheduleLayoutForChildWidgets();
 
                     getLayoutManager().layoutNow();
                 }
