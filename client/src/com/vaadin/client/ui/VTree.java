a212ea87b906e737bb9557b2204d27faf6ccc69f
Yuriy Artamonov <artamonov@haulmont.com>
2015-04-10 13:58:54 +0400

    Protected API for proper extension of VTree

--- client/src/com/vaadin/client/ui/VTree.java
+++ client/src/com/vaadin/client/ui/VTree.java
@@ -351,7 +351,8 @@ public class VTree extends FocusElementPanel implements VHasDropHandler,
     }
 
     private String findCurrentMouseOverKey(Element elementOver) {
-        TreeNode treeNode = WidgetUtil.findWidget(elementOver, TreeNode.class);
+        // Haulmont API dependency
+        TreeNode treeNode = WidgetUtil.findWidget(elementOver, getTreeNodeClass());
         return treeNode == null ? null : treeNode.key;
     }
 
@@ -2163,6 +2164,11 @@ public class VTree extends FocusElementPanel implements VHasDropHandler,
         return null;
     }
 
+    // Haulmont API
+    protected Class<? extends Widget> getTreeNodeClass() {
+        return TreeNode.class;
+    }
+
     /*
      * (non-Javadoc)
      * 
@@ -2188,7 +2194,8 @@ public class VTree extends FocusElementPanel implements VHasDropHandler,
             return "fe";
         }
 
-        TreeNode treeNode = WidgetUtil.findWidget(subElement, TreeNode.class);
+        // Haulmont API dependency
+        TreeNode treeNode = WidgetUtil.findWidget(subElement, getTreeNodeClass());
         if (treeNode == null) {
             // Did not click on a node, let somebody else take care of the
             // locator string
