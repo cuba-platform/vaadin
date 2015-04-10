19213460b18ad64be8ac3c7d1ecaa85436758faf
Yuriy Artamonov <artamonov@haulmont.com>
2015-04-10 13:47:47 +0400

    Fix for #12970, initial layout for cell widgets

--- client/src/com/vaadin/client/ui/table/TableConnector.java
+++ client/src/com/vaadin/client/ui/table/TableConnector.java
@@ -21,6 +21,7 @@ import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style.Position;
+import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.Widget;
 import com.vaadin.client.ApplicationConnection;
 import com.vaadin.client.BrowserInfo;
@@ -32,7 +33,9 @@ import com.vaadin.client.ServerConnector;
 import com.vaadin.client.TooltipInfo;
 import com.vaadin.client.UIDL;
 import com.vaadin.client.WidgetUtil;
+import com.vaadin.client.*;
 import com.vaadin.client.ui.AbstractHasComponentsConnector;
+import com.vaadin.client.ui.ManagedLayout;
 import com.vaadin.client.ui.PostLayoutListener;
 import com.vaadin.client.ui.VScrollTable;
 import com.vaadin.client.ui.VScrollTable.ContextMenuDetails;
@@ -352,6 +355,22 @@ public class TableConnector extends AbstractHasComponentsConnector implements
                     }
                     getLayoutManager().setNeedsVerticalLayout(
                             TableConnector.this);
+
+                    // Fix for #VAADIN-12970, initial layout for cell widgets
+                    // Haulmont API
+                    for (Widget w : getWidget().scrollBody) {
+                        HasWidgets row = (HasWidgets) w;
+                        for (Widget child : row) {
+                            ComponentConnector childConnector =
+                                    Util.findConnectorFor(child);
+                            if (childConnector instanceof ManagedLayout) {
+                                LayoutManager lm = getLayoutManager();
+                                lm.setNeedsMeasure(childConnector);
+                                lm.setNeedsLayout((ManagedLayout) childConnector);
+                            }
+                        }
+                    }
+
                     getLayoutManager().layoutNow();
                 }
             });
