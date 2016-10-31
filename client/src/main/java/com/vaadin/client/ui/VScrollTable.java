717eb7c379d40b113a0e39881ad10011f137a6a9
Daniil Tsarev <tsarev@haulmont.com>
2016-10-31 14:05:31 +0400

    Ability to check columns before send updated width to server (artamonov)

--- client/src/main/java/com/vaadin/client/ui/VScrollTable.java
+++ client/src/main/java/com/vaadin/client/ui/VScrollTable.java
@@ -971,6 +971,11 @@ public class VScrollTable extends FlowPanel
 
     }
 
+    // Haulmont API
+    protected boolean checkColumnForUpdateWidth(HeaderCell cell) {
+        return true;
+    }
+
     /**
      * Non-immediate variable update of column widths for a collection of
      * columns.
@@ -978,11 +983,14 @@ public class VScrollTable extends FlowPanel
      * @param columns
      *            the columns to trigger the events for.
      */
-    private void sendColumnWidthUpdates(Collection<HeaderCell> columns) {
+    protected void sendColumnWidthUpdates(Collection<HeaderCell> columns) {
         String[] newSizes = new String[columns.size()];
         int ix = 0;
         for (HeaderCell cell : columns) {
-            newSizes[ix++] = cell.getColKey() + ":" + cell.getWidth();
+            // Haulmont API call
+            if (checkColumnForUpdateWidth(cell)) {
+                newSizes[ix++] = cell.getColKey() + ":" + cell.getWidth();
+            }
         }
         client.updateVariable(paintableId, "columnWidthUpdates", newSizes,
                 false);
