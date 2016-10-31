cbb39aa66ce39433816909cb7f7a50bb22c9c50f
Daniil Tsarev <tsarev@haulmont.com>
2016-10-31 14:56:03 +0400

    Support for double click in VTree (artamonov)

--- client/src/main/java/com/vaadin/client/ui/VTree.java
+++ client/src/main/java/com/vaadin/client/ui/VTree.java
@@ -162,7 +162,8 @@ public class VTree extends FocusElementPanel
     /** For internal use only. May be removed or replaced in the future. */
     public int dragMode;
 
-    private boolean selectionHasChanged = false;
+    // Haulmont API dependency
+    protected boolean selectionHasChanged = false;
 
     /*
      * to fix #14388. The cause of defect #14388: event 'clickEvent' is sent to
@@ -482,6 +483,9 @@ public class VTree extends FocusElementPanel
      * Sends the selection to the server
      */
     private void sendSelectionToServer() {
+        // Haulmont API
+        final boolean immediateValue = immediate;
+
         Command command = new Command() {
             @Override
             public void execute() {
@@ -664,6 +668,10 @@ public class VTree extends FocusElementPanel
 
                 @Override
                 public void execute() {
+                    // Haulmont API
+                    boolean needToHandleClick = isNeedToHandleClick();
+                    if (!needToHandleClick)
+                        return;
 
                     if (multiSelectMode == MultiSelectMode.SIMPLE
                             || !isMultiselect) {
@@ -698,6 +706,11 @@ public class VTree extends FocusElementPanel
                 }
             });
 
+            return true;
+        }
+
+        // Haulmont API
+        protected boolean isNeedToHandleClick() {
             return true;
         }
 
@@ -823,7 +836,8 @@ public class VTree extends FocusElementPanel
                     || (icon != null && target == icon.getElement()));
         }
 
-        private void fireClick(final Event evt) {
+        // Haulmont API dependency
+        protected void fireClick(final Event evt) {
             /*
              * Ensure we have focus in tree before sending variables. Otherwise
              * previously modified field may contain dirty variables.
@@ -842,6 +856,10 @@ public class VTree extends FocusElementPanel
 
             final MouseEventDetails details = MouseEventDetailsBuilder
                     .buildMouseEventDetails(evt);
+
+            // Haulmont API
+            final int eventType = DOM.eventGetType(evt);
+            prepareToFireClick(eventType);
 
             executeEventCommand(new ScheduledCommand() {
 
@@ -867,19 +885,33 @@ public class VTree extends FocusElementPanel
                             clickEventPending = false;
                         }
                     }
-                    client.updateVariable(paintableId, "clickedKey", key,
-                            false);
+
+                    // Haulmont API
+                    clickEventPending = isNeedToSendDoubleClick(eventType, clickEventPending);
+
+                    client.updateVariable(paintableId, "clickedKey", key, false);
                     client.updateVariable(paintableId, "clickEvent",
-                            details.toString(), !clickEventPending);
+                            details.toString(), clickEventPending);
                 }
             });
+        }
+
+        // Haulmont API
+        protected void prepareToFireClick(int eventType) {
+        }
+
+        // Haulmont API
+        protected boolean isNeedToSendDoubleClick(int eventType, boolean sendClickEventNow) {
+            return sendClickEventNow;
         }
 
         /*
          * Must wait for Safari to focus before sending click and value change
          * events (see #6373, #6374)
+         *
+         * Haulmont API dependency
          */
-        private void executeEventCommand(ScheduledCommand command) {
+        protected void executeEventCommand(ScheduledCommand command) {
             if (BrowserInfo.get().isWebkit() && !treeHasFocus) {
                 Scheduler.get().scheduleDeferred(command);
             } else {
@@ -887,7 +919,8 @@ public class VTree extends FocusElementPanel
             }
         }
 
-        private void toggleSelection() {
+        // Haulmont API dependency
+        protected void toggleSelection() {
             if (selectable) {
                 VTree.this.setSelected(this, !isSelected());
             }
