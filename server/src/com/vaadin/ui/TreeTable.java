efeb093b4bfe6ab052e11b2049493c1d2bb50ec3
Yuriy Artamonov <artamonov@haulmont.com>
2016-12-30 19:19:11 +0400

    Haulmont API for TreeTable expandAll / collapseAll

--- server/src/com/vaadin/ui/TreeTable.java
+++ server/src/com/vaadin/ui/TreeTable.java
@@ -17,12 +17,7 @@
 package com.vaadin.ui;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
