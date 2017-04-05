5d3068f7ed1499b49cd938210dade21459e19fef
Yuriy Artamonov <artamonov@haulmont.com>
2017-04-05 12:45:50 +0400

    PushHandler - Decrease log level of warning about atmosphere connection lookup to FINE

--- server/src/main/java/com/vaadin/server/communication/PushHandler.java
+++ server/src/main/java/com/vaadin/server/communication/PushHandler.java
@@ -368,7 +368,7 @@ public class PushHandler {
                                     + " see http://dev.vaadin.com/ticket/14251.");
                     return;
                 } else {
-                    getLogger().log(Level.INFO,
+                    getLogger().log(Level.FINE,
                             "No UI was found based on data in the request,"
                                     + " but a slower lookup based on the AtmosphereResource succeeded."
                                     + " See http://dev.vaadin.com/ticket/14251 for more details.");
