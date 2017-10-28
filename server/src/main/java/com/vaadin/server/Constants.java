26151717cbd5bf04534edfbf423a95c042b68ec3
Yuriy Artamonov <artamonov@haulmont.com>
2017-10-28 16:50:06 +0400

    Use org.atmosphere:atmosphere-runtime:2.4.19.cuba.0

--- server/src/main/java/com/vaadin/server/Constants.java
+++ server/src/main/java/com/vaadin/server/Constants.java
@@ -66,7 +66,7 @@ public interface Constants {
             + "=================================================================";
 
     // Keep the version number in sync with pom.xml
-    static final String REQUIRED_ATMOSPHERE_RUNTIME_VERSION = "2.4.11";
+    static final String REQUIRED_ATMOSPHERE_RUNTIME_VERSION = "2.4.19.cuba.0";
 
     static final String INVALID_ATMOSPHERE_VERSION_WARNING = "\n"
             + "=================================================================\n"
