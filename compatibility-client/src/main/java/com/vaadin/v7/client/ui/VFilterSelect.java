1d4921c1e6980e38a830353e923492346711b814
Yuriy Artamonov <artamonov@haulmont.com>
2017-12-10 18:49:39 +0400

    HasFocusHandlers for VFilterSelect

--- compatibility-client/src/main/java/com/vaadin/v7/client/ui/VFilterSelect.java
+++ compatibility-client/src/main/java/com/vaadin/v7/client/ui/VFilterSelect.java
@@ -37,21 +37,10 @@ import com.google.gwt.dom.client.Style;
 import com.google.gwt.dom.client.Style.Display;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.dom.client.Style.Visibility;
-import com.google.gwt.event.dom.client.BlurEvent;
-import com.google.gwt.event.dom.client.BlurHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.FocusEvent;
-import com.google.gwt.event.dom.client.FocusHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyDownEvent;
-import com.google.gwt.event.dom.client.KeyDownHandler;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.event.dom.client.LoadEvent;
-import com.google.gwt.event.dom.client.LoadHandler;
+import com.google.gwt.event.dom.client.*;
 import com.google.gwt.event.logical.shared.CloseEvent;
 import com.google.gwt.event.logical.shared.CloseHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.i18n.client.HasDirection.Direction;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.DOM;
@@ -101,7 +90,7 @@ import com.vaadin.v7.shared.ui.combobox.FilteringMode;
 public class VFilterSelect extends Composite
         implements Field, KeyDownHandler, KeyUpHandler, ClickHandler,
         FocusHandler, BlurHandler, Focusable, SubPartAware, HandlesAriaCaption,
-        HandlesAriaInvalid, HandlesAriaRequired, DeferredWorker {
+        HandlesAriaInvalid, HandlesAriaRequired, DeferredWorker, HasFocusHandlers {
 
 
     /**
@@ -1415,6 +1404,12 @@ public class VFilterSelect extends Composite
     private boolean enableDebug = false;
 
     private final FlowPanel panel = new FlowPanel();
+
+    // Haulmont API
+    @Override
+    public HandlerRegistration addFocusHandler(FocusHandler handler) {
+        return tb.addFocusHandler(handler);
+    }
 
     /**
      * The text box where the filter is written
