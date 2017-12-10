518e8cec00d8103fdd3ec93a7a9cf6c0cc3f48ea
Yuriy Artamonov <artamonov@haulmont.com>
2017-12-10 20:21:16 +0400

    VFilterSelect protected API

--- compatibility-client/src/main/java/com/vaadin/v7/client/ui/VFilterSelect.java
+++ compatibility-client/src/main/java/com/vaadin/v7/client/ui/VFilterSelect.java
@@ -2635,8 +2635,10 @@ public class VFilterSelect extends Composite
 
     /*
      * Anything that should be set after the client updates the server.
+     *
+     * Haulmont API dependency
      */
-    private void afterUpdateClientVariables() {
+    protected void afterUpdateClientVariables() {
         // We need this here to be consistent with the all the calls.
         // Then set your specific selection type only after
         // client.updateVariable() method call.
