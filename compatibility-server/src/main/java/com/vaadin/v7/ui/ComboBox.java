51fbe7e03485f44ece5ae8d1153df147fa5d1d11
Yuriy Artamonov <artamonov@haulmont.com>
2017-12-10 20:25:39 +0400

    Do not throw ReadOnlyException for ComboBox change after long operation

--- compatibility-server/src/main/java/com/vaadin/v7/ui/ComboBox.java
+++ compatibility-server/src/main/java/com/vaadin/v7/ui/ComboBox.java
@@ -731,7 +731,8 @@ public class ComboBox extends AbstractSelect
         // component hierarchy
 
         // Selection change
-        if (variables.containsKey("selected")) {
+        // Haulmont API dependency
+        if (variables.containsKey("selected") && !isReadOnly()) {
             final String[] ka = (String[]) variables.get("selected");
 
             // Single select mode
@@ -751,6 +752,9 @@ public class ComboBox extends AbstractSelect
                     setValue(id, true);
                 }
             }
+        } else if (variables.containsKey("selected")) {
+            // Haulmont API dependency
+            markAsDirty();
         }
 
         String newFilter;
