3318ed5481745734b1c7026d7b9d642cb867cdd2
Yuriy Artamonov <artamonov@haulmont.com>
2017-12-10 22:10:52 +0400

    PL-9305 Revert default access level for Grid.focusCell method

--- compatibility-client/src/main/java/com/vaadin/v7/client/widgets/Grid.java
+++ compatibility-client/src/main/java/com/vaadin/v7/client/widgets/Grid.java
@@ -6337,8 +6337,7 @@ public class Grid<T> extends ResizeComposite implements HasSelectionHandlers<T>,
      * @param columnIndexDOM
      *            index (excluding hidden columns) of cell to focus
      */
-    // Haulmont API
-    public void focusCell(int rowIndex, int columnIndexDOM) {
+    void focusCell(int rowIndex, int columnIndexDOM) {
         final Range rowRange = Range.between(0, dataSource.size());
         final Range columnRange = Range.between(0, getVisibleColumns().size());
 
@@ -6354,6 +6353,15 @@ public class Grid<T> extends ResizeComposite implements HasSelectionHandlers<T>,
                     escalator.getBody());
             WidgetUtil.focus(getElement());
         }
+    }
+
+    /**
+     * Haulmont API.
+     * <p>
+     * Public method for accessing {@link #focusCell(int, int)} method.
+     */
+    public void focusBodyCell(int rowIndex, int columnIndexDOM) {
+        focusCell(rowIndex, columnIndexDOM);
     }
 
     /**
